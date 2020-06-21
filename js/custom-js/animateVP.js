822feeb60f6af21c067db67d42ad8d4f2c4f1b91
Stein-Hakase <fakhriddine.hariz@gmail.com>
2020-06-21 17:57:35 +0700

    Adding animations for header , features and screenshots

---
+++ js/custom-js/animateVP.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    // Check if element is scrolled into view
+    function isScrolledIntoView(elem) {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+  
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(elem).height();
+  
+      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+    // If element is scrolled into view, fade it in
+    $(window).scroll(function() {
+      $('.scroll-animations .animate__animated').each(function() {
+        if (isScrolledIntoView(this) === true) {
+          $(this).addClass('animate__fadeInLeft ');
+        }
+      });
+    });
+  });
